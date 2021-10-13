18f7b45faa861929962ea179f276b2247a2cf2df
hansolShin <hansolstar@naver.com>
2021-10-13 18:36:50 +0900

    feature: get current user name

--- src/main/java/server/server/controller/pageController.java
+++ src/main/java/server/server/controller/pageController.java
@@ -1,4 +1,30 @@
 package server.server.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import server.server.memberDto.ApiResponse;
+import server.server.service.MemberService;
+
+import javax.servlet.http.HttpServletRequest;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/api/page")
+@RequiredArgsConstructor
+@Slf4j
 public class pageController {
+    @Autowired
+    private final MemberService memberService;
+
+    @GetMapping("/user")
+    public String getCurrentUserName(HttpServletRequest request){
+        return memberService.getCurrentUserInfo(request).get().getName();
+    }
 }
